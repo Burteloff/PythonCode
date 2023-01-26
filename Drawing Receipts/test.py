a4645a0d8364f6d5f6d0f939a408a1d8dc0470e5
Burteloff <38983111+Burteloff@users.noreply.github.com>
2023-01-27 02:30:35 +0300

    edit test

--- Drawing Receipts/test.py
+++ Drawing Receipts/test.py
@@ -1,13 +1,22 @@
 import unittest
 from PIL import Image
+import configparser
 import os
 from main import ReceiptController,ReceiptModel,ReceiptView
 
 class TestReceiptController(unittest.TestCase):
     def setUp(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
         self.time = "12:00 PM"
         self.location_name = "Store Name"
         self.location_address = "123 Main St"
+
+        self.width=int(self.config['image']['width'])
+        self.height=int(self.config['image']['height'])
+        self.color=tuple(map(int,self.config['image']['color'].split(',')))
+       
+
         self.items = ["Item 1", "Item 2", "Item 3"]
         self.receipt_controller = ReceiptController()
 
@@ -21,9 +30,9 @@ class TestReceiptController(unittest.TestCase):
         self.assertTrue(os.path.isfile("receipt.png"))
         Image.MAX_IMAGE_PIXELS = 100000000
         img = Image.open("receipt.png")
-        img.thumbnail((80,180))
-        self.assertEqual(img.size, (31, 180))
-        self.assertEqual(img.getpixel((0, 0)), (255, 255, 255))
+   
+        self.assertEqual(img.size, ((self.width),self.height))
+        self.assertEqual(img.getpixel((0, 0)), (self.color))
 
 class TestReceiptModel(unittest.TestCase):
     def setUp(self):
@@ -42,12 +51,15 @@ class TestReceiptView(unittest.TestCase):
     def setUp(self):
         self.receipt_model = ReceiptModel("12:00 PM", "Store Name", "123 Main St", ["Item 1", "Item 2", "Item 3"])
         self.receipt_view = ReceiptView()
+        self.width=int(self.config['image']['width'])
+        self.height=int(self.config['image']['height'])
+        self.color=tuple(map(int,self.config['image']['color'].split(',')))
 
     def test_create_receipt(self):
         self.receipt_view.create_receipt(self.receipt_model)
         self.assertTrue(os.path.isfile("receipt.png"))
         Image.MAX_IMAGE_PIXELS = 100000000
         img = Image.open("receipt.png")
-        img.thumbnail((80,180))
-        self.assertEqual(img.size, (31, 180))
-        self.assertEqual(img.getpixel((0, 0)), (254, 254, 254))
+        
+        self.assertEqual(img.size, ((self.width),self.height))
+        self.assertEqual(img.getpixel((0, 0)), (self.color))
